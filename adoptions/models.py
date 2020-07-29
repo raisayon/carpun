3bbeb373925861475a46acb9d8e068b20329711c
breaker2l <34549956+breaker2l@users.noreply.github.com>
2020-07-29 10:02:28 -0700

    Update models.py

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Pet(models.Model):
+    SEX_CHOICES = [('M','Male'),('F','Female')] #() tuple
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30)
+    description = models.TextField(max_length=100)
+    sex = models.CharField(max_length=1,choices=SEX_CHOICES,blank=True)
+    submission_date = models.DateTimeField()
+    age = models.ImageField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine',blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
